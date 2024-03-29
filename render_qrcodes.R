a1d60b0a14020bcc0eae6c9618e9b6681abb9cc0
Julie Johannessen <113525639+julieaaj@users.noreply.github.com>
2024-03-29 22:14:11 +0100

    ud

--- render_qrcodes.R
+++ render_qrcodes.R
@@ -7,4 +7,6 @@ for (i in 1:5) {
   url <- paste0("https://julieaaj.github.io/rebus_page/pasw_qr_", i, ".html")
   qr_code <- qr_code(url)
   plot(qr_code)
+  # Add a title
+  title(main = paste("Post", i))
 }
