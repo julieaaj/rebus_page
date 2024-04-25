d7bfd2820675c104cd780c8040b48b74ffa28562
Julie Johannessen <113525639+julieaaj@users.noreply.github.com>
2024-04-25 20:34:12 +0200

    test fix

--- render_qrcodes.R
+++ render_qrcodes.R
@@ -3,7 +3,7 @@ library(qrcode)
 # geneerere QR koder for landingssidene
 
 # Generate QR codes
-for (i in 1:5) {
+for (i in 1:8) {
   url <- paste0("https://julieaaj.github.io/rebus_page/pasw_qr_", i, ".html")
   qr_code <- qr_code(url)
   plot(qr_code)
