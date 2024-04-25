9986e6928c9e5c2d5e61fdece10d56d0cb7d4ea4
Julie Johannessen <113525639+julieaaj@users.noreply.github.com>
2024-04-25 22:23:18 +0200

    fix order of posts

--- taskRedirect.js
+++ taskRedirect.js
@@ -1,19 +1,20 @@
 // Define passwords for each team (ensure security in a real application)
 const teamPasswords = {
-    "team1": "1",
-    "team2": "2",
-    "team3": "3"
+    "team1": "Ostepop",
+    "team2": "Hestesko",
+    "team3": "Oskarkatt"
 };
 
 // Define the task map for each team; maps post filenames to tasks
 const taskMap = {
-    "team1": {"post4": 1, "post2": 2, "post7": 3, "post1": 4, "post5": 5, "post3": 6, "post6": 7, "post8": 8},
-    "team2": {"post1": 1, "post3": 2, "post2": 3, "post5": 4, "post4": 5, "post6": 6, "post7": 7, "post8": 8},
-    "team3": {"post2": 1, "post4": 2, "post1": 3, "post3": 4, "post7": 5, "post5": 6, "post6": 7, "post8": 8}
+    "team1": {"post3": 1, "post6": 2, "post1": 3, "post7": 4, "post2": 5, "post5": 6, "post4": 7, "post8": 8},
+    "team2": {"post2": 1, "post5": 2, "post3": 3, "post1": 4, "post6": 5, "post4": 6, "post7": 7, "post8": 8},
+    "team3": {"post1": 1, "post4": 2, "post2": 3, "post5": 4, "post7": 5, "post6": 6, "post3": 7, "post8": 8}
 };
 
+
 function promptPassword(team) {
-    var password = prompt("Please enter your password:", "");
+    var password = prompt("Skriv lag-passordet deres:", "");
     verifyTeam(team, password);
 }
 
@@ -21,7 +22,7 @@ function verifyTeam(team, password) {
     if (teamPasswords[team] && teamPasswords[team] === password) {
         redirectToTask(team);
     } else {
-        alert("Incorrect password. Please try again.");
+        alert("Feil passord. Det burde ikke være så jævlig vanskelig, faktisk. U had 1 job?.");
     }
 }
 
